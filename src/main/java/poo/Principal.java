31ed1f97214085b6e2810e7c4a917950f9110013
alexandre <alexandre.hc@aluno.ifsc.edu.br>
2019-10-03 08:00:33 -0300

    basico de JavaDoc

---
+++ src/main/java/poo/Principal.java
@@ -0,0 +1,22 @@
+package poo;
+
+// Settings -> editor -> General -> COde completion     Marcar show the documentation popup  in
+
+
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+
+
+        SomaInteiros s = new SomaInteiros();
+
+        s.soma(1,2);
+
+
+
+
+    }
+}
---
+++ src/main/java/poo/SomaInteiros.java
@@ -0,0 +1,17 @@
+package poo;
+
+public class SomaInteiros {
+
+    /**
+     * Faz soma de inteiros
+     * @param a operando 1
+     * @param b operando 2
+     * @return a soma deles
+     */
+
+    public int soma(int a, int b){
+        return a + b;
+    }
+
+
+}
